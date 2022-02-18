374d4c8fe63f7b5d68ea50668217883c3618043f
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-02-18 11:29:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./index.css";
 import Files from "./components/Files";
+import Storage from "./components/Storage";
 
 function App() {
   return (
-    <div className="App mx-auto mt-[7.938rem] flex h-[25.813rem] w-[20.375rem] flex-col gap-4">
+    <main className="App mx-auto mt-[7.938rem] flex h-[25.813rem] w-[20.375rem] flex-col gap-4">
       <Files />
-    </div>
+      <Storage />
+    </main>
   );
 }
 
