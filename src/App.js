7cca872c50a88c617c30e68a9e79655e4ba642a6
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-02-18 14:30:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Storage from "./components/Storage";
 
 function App() {
   return (
-    <main className="z-10 mx-auto mb-[7.938rem] mt-[7.938rem] flex h-[25.813rem] w-[20.375rem] flex-col gap-4 xl:mb-[19.375rem] xl:mt-[18.063rem] xl:h-[12.563rem]  xl:w-[57.5rem] xl:flex-row">
+    <main className="z-10 mx-auto mb-[7.938rem] mt-[7.938rem] flex h-[25.813rem] w-[20.375rem] flex-col gap-4 xl:mb-[19.375rem] xl:mt-[18.063rem] xl:h-[12.563rem] xl:w-[57.5rem]  xl:flex-row xl:gap-[1.875rem]">
       <Files />
       <Storage />
     </main>
